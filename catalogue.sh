579249b8fc254e02a8b5045c585b229061ad4514
sivakrishna <sivakrishn17@gmail.com>
2023-08-26 13:54:55 +0530

    Shell Scripting on Roboshop shell

--- catalogue.sh
+++ catalogue.sh
@@ -60,7 +60,7 @@ npm install &>>$LOGFILE
 VALIDATE $? "Installing dependencies"
 
 # give full path of catalogue.service because we are inside /app
-cp \devopslearn\draw.io\repos\roboshop-shell /etc/systemd/system/catalogue.service &>>$LOGFILE
+cp C:\devopslearn\draw.io\repos\roboshop-shell /etc/systemd/system/catalogue.service &>>$LOGFILE
 
 VALIDATE $? "copying catalogue.service"
 
