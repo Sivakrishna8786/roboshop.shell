8c0cc25b7427270ab46f5b9cb4ff0a35e0bc0569
sivakrishna <sivakrishn17@gmail.com>
2023-08-26 12:24:55 +0530

    Shell Scripting on Roboshop shell

--- catalogue.sh
+++ catalogue.sh
@@ -2,64 +2,67 @@
 
 DATE=$(date +%F)
 LOGSDIR=/tmp
+# /home/centos/shellscript-logs/script-name-date.log
 SCRIPT_NAME=$0
 LOGFILE=$LOGSDIR/$0-$DATE.log
 USERID=$(id -u)
- 
 R="\e[31m"
 G="\e[32m"
-Y="\e[33m"
 N="\e[0m"
+Y="\e[33m"
 
-   if [ $USERID -ne 0 ];
-   then 
-       echo -e "$R ERROR:: Please run this script with root user $N"
-       exit 1
-    fi
+if [ $USERID -ne 0 ];
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1
+fi
 
 VALIDATE(){
     if [ $1 -ne 0 ];
-    then 
+    then
         echo -e "$2 ... $R FAILURE $N"
         exit 1
     else
         echo -e "$2 ... $G SUCCESS $N"
-    fi 
+    fi
 }
 
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOGFILE
 
-VALIDATE $? "stasetting up NPM source" 
+VALIDATE $? "Setting up NPM Source"
 
 yum install nodejs -y &>>$LOGFILE
 
-VALIDATE $? "Installing nodejs"
+VALIDATE $? "Installing NodeJS"
 
+#once the user is created, if you run this script 2nd time
+# this command will defnitely fail
+# IMPROVEMENT: first check the user already exist or not, if not exist then create
 useradd roboshop &>>$LOGFILE
 
+#write a condition to check directory already exist or not
 mkdir /app &>>$LOGFILE
 
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>$LOGFILE
 
 VALIDATE $? "downloading catalogue artifact"
 
-cd /app   
+cd /app &>>$LOGFILE
 
-VALIDATE $? "moving into app directory"
+VALIDATE $? "Moving into app directory"
 
 unzip /tmp/catalogue.zip &>>$LOGFILE
 
 VALIDATE $? "unzipping catalogue"
 
-cd /app
-
-npm install  &>>$LOGFILE
+npm install &>>$LOGFILE
 
 VALIDATE $? "Installing dependencies"
 
-cp C:\devopslearn\draw.io\repos\roboshop-shell\catalogue.services /etc/systemd/system/catalogue.service &>>$LOGFILE
+# give full path of catalogue.service because we are inside /app
+cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>>$LOGFILE
 
-VALIDATE $? "copying catalogue services"
+VALIDATE $? "copying catalogue.service"
 
 systemctl daemon-reload &>>$LOGFILE
 
@@ -67,20 +70,20 @@ VALIDATE $? "daemon reload"
 
 systemctl enable catalogue &>>$LOGFILE
 
-VALIDATE $? "enable catalogue"
+VALIDATE $? "Enabling Catalogue"
 
 systemctl start catalogue &>>$LOGFILE
 
-VALIDATE $? "start catalogue"
+VALIDATE $? "Starting Catalogue"
 
-cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE
 
-VALIDATE $? "mongo client"
+VALIDATE $? "Copying mongo repo"
 
-yum install mongodb-org-shell -y &>> $LOGFILE
+yum install mongodb-org-shell -y &>>$LOGFILE
 
-VALIDATE $? "install mongo client"
+VALIDATE $? "Installing mongo client"
 
-mongo --host mongodb.devopslearning.online </app/schema/catalogue.js &>> $LOGFILE
+mongo --host mongodb.devopslearning.online </app/schema/catalogue.js &>>$LOGFILE
 
 VALIDATE $? "loading catalogue data into mongodb"
