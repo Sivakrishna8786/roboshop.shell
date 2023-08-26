aceb8eaf01fda74b7dc513c01630fdf4fff9d82e
sivakrishna <sivakrishn17@gmail.com>
2023-08-26 19:20:56 +0530

    Shell Scripting on Roboshop shell

--- cataloue.sh
+++ cataloue.sh
@@ -60,7 +60,7 @@ npm install &>>$LOGFILE
 VALIDATE $? "Installing dependencies"
 
 # give full path of catalogue.service because we are inside /app
-cp \\devopslearn\\draw.io\\repos\\roboshop-shell\\catalogue.service /etc/systemd/system/catalogue.service &>>$LOGFILE
+cp C:\\devopslearn\\draw.io\\repos\\roboshop-shell\\catalogue.service /etc/systemd/system/catalogue.service &>>$LOGFILE
 
 VALIDATE $? "copying catalogue.service"
 
@@ -76,7 +76,7 @@ systemctl start catalogue &>>$LOGFILE
 
 VALIDATE $? "Starting Catalogue"
 
-cp /C:/devopslearn/draw.io/repos/roboshop-shell  /etc/yum.repos.d/mongo.repo &>>$LOGFILE
+cp C:\\devopslearn\\draw.io\\repos\\roboshop-shell\\mongo.repo  /etc/yum.repos.d/mongo.repo &>>$LOGFILE
 
 VALIDATE $? "Copying mongo repo"
 
